abe8c7d78309d53c5e650ab35f49eb12b77503e7
Gareth Jones <gareth@garethpaul.com>
2016-01-06 17:09:15 -0800

    Adding initial

---
+++ iHeartRating/iHeartRating.h
@@ -0,0 +1,19 @@
+//
+//  iHeartRating.h
+//  iHeartRating
+//
+//  Created by Gareth Jones on 1/6/16.
+//  Copyright © 2016 GPJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iHeartRating.
+FOUNDATION_EXPORT double iHeartRatingVersionNumber;
+
+//! Project version string for iHeartRating.
+FOUNDATION_EXPORT const unsigned char iHeartRatingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iHeartRating/PublicHeader.h>
+
+
